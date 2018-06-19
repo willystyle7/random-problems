3723b9f6f3ea2f29db7df59d12eddf85d17710be
willystyle7 <ilia111@yahoo.com>
2018-06-19 16:06:03 +0300

    Round problem with modulus

--- ProblemRounding/problemRounding.cs
+++ ProblemRounding/problemRounding.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Numerics;
+//using System.Numerics;
 
 class problemRounding
 {
@@ -17,13 +14,26 @@ class problemRounding
         //Console.WriteLine(result);
 
         //Decision for N > 0 and N < 1000
-        BigInteger a = BigInteger.Parse(Console.ReadLine());
-        BigInteger b = BigInteger.Parse(Console.ReadLine());
+        //BigInteger a = BigInteger.Parse(Console.ReadLine());
+        //BigInteger b = BigInteger.Parse(Console.ReadLine());
+        //int n = int.Parse(Console.ReadLine());
+        //BigInteger result = new BigInteger();
+        //result = BigInteger.Divide(BigInteger.Multiply(a, BigInteger.Pow(10, n)), b);
+        //string finalResult = result.ToString();
+        //finalResult = finalResult.Insert((finalResult.Length - n), ".");
+        //Console.WriteLine(finalResult);
+		
+		//Decision with modulus
+		int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
         int n = int.Parse(Console.ReadLine());
-        BigInteger result = new BigInteger();
-        result = BigInteger.Divide(BigInteger.Multiply(a, BigInteger.Pow(10, n)), b);
-        string finalResult = result.ToString();
-        finalResult = finalResult.Insert((finalResult.Length - n), ".");
-        Console.WriteLine(finalResult);
+        string result = (a / b) + ".";
+        for (int i = 0; i < n; ++i)
+        {
+            a %= b;
+            a *= 10;
+            result += a / b;
+        }
+        Console.WriteLine(result);
     }
 }
